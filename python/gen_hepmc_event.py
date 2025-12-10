699b13efe5e11fea400f286a2e0d31e3ce7e455e
simonge <simon.gardner@glasgow.ac.uk>
2025-12-10 14:21:35 +0000

    Add non-interacting beam simple event

--- python/gen_hepmc_event.py
+++ python/gen_hepmc_event.py
@@ -29,6 +29,42 @@ def create_zonly_event(file_name):
     hepmc3_out.write_event(evt)
     hepmc3_out.close()
 
+def create_simple_event(file_name, n_events=1000, file_type="ascii", electron_energy=18, proton_energy=275):
+
+    writer = None
+    if file_type == "ascii":
+        writer = hm.WriterAscii(file_name)
+    elif file_type == "root":
+        writer = hm.WriterRoot(file_name)
+
+    for i in range(n_events):
+        evt = hm.GenEvent(hm.Units.GEV, hm.Units.MM)
+
+        evt.set_event_number(i)
+
+        v1 = hm.GenVertex(hm.FourVector(0, 0, 0, 0))
+
+        p1 = hm.GenParticle(hm.FourVector(0, 0, proton_energy, proton_energy), 2212, 4)
+        p2 = hm.GenParticle(hm.FourVector(0, 0, -electron_energy, electron_energy), 11, 4)
+        
+        p3 = hm.GenParticle(hm.FourVector(0, 0, proton_energy, proton_energy), 2212, 1)
+        p4 = hm.GenParticle(hm.FourVector(0, 0, -electron_energy, electron_energy), 11, 1)
+
+        v1.add_particle_in(p1)
+        v1.add_particle_in(p2)
+
+        v1.add_particle_out(p3)
+        v1.add_particle_out(p4)
+
+        evt.add_vertex(v1)
+        evt.add_particle(p1)        
+        evt.add_particle(p2)
+        evt.add_particle(p3)
+        evt.add_particle(p4)
+        writer.write_event(evt)
+
+    writer.close()
+
 
 def read_events(file_name):
     input_file = hm.ReaderAscii(file_name)
@@ -58,5 +94,7 @@ def read_events(file_name):
 
 
 if __name__ == "__main__":
+    # Generate some simple events to ensure the afterburner can read them
+    create_simple_event("beam_18x275_events.hepmc", n_events=10, file_type="ascii", electron_energy=18, proton_energy=275)
     create_zonly_event("zonly_event.hepmc")
     read_events("zonly_event.hepmc")
